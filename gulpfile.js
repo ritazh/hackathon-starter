c07bdaa4cb6757f028c02e01ed205662e5062d24
Rita Zhang <rita.z.zhang@gmail.com>
2016-03-25 15:10:03 -0700

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+// include plug-ins
+var jshint = require('gulp-jshint');
+
+// JS hint task
+gulp.task('jshint', function() {
+  gulp.src('./*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
 gulp.task('default', function() {
-  // place code for your default task here
+  ///TODO
 });
